5b85f4d4185205a04ae6f5fce9bb9e2db6cfa56b
Lilian Galadima <anyagaladima@gmail.com>
2023-08-14 00:42:36 +0100

    formatted file

--- s3-bucket-state/bucket.tf
+++ s3-bucket-state/bucket.tf
@@ -1,6 +1,6 @@
 # Create private S3 bucket that stores terraform state 
 resource "aws_s3_bucket" "tfstate" {
-  bucket = "wordpress-3tier-state-files"
+  bucket        = "wordpress-3tier-state-files"
   force_destroy = true
 }
 
@@ -13,8 +13,8 @@ resource "aws_s3_bucket_ownership_controls" "s3_ownership" {
 
 resource "aws_s3_bucket_acl" "tfstate_acl" {
   depends_on = [aws_s3_bucket_ownership_controls.s3_ownership]
-  bucket = aws_s3_bucket.tfstate.id
-  acl    = "private"
+  bucket     = aws_s3_bucket.tfstate.id
+  acl        = "private"
 }
 
 # Add bucket versioning for state rollback
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption"
   bucket = aws_s3_bucket.tfstate.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
   }
 }
